588f9ac1733c6c5ec2e53b92be9128db749177a6
alfdid0406 <alfdid0406@gmail.com>
2024-04-25 17:26:48 +0900

    commit message

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from apps.app import db
+from apps.crud.models import User
+from apps.crud.forms import UserForm
+from flask import Blueprint, render_template, redirect, url_for
 
 crud = Blueprint(
     "crud",
@@ -10,3 +13,74 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    # result = db.session.query(User).first()
+    # print(result)
+    # User.query.all()
+    user = User(
+        username="박정훈",
+        email="alfdid0406@gmail.com",
+        password="123456"
+    )
+    db.session.add(user)
+    db.session.commit()
+
+    return "콘솔 로그를 확인해 주세요"
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    pass
+
+# 121 페이지부터 132 페이지까지 작업을 진행하자!
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    # UserForm을 인스턴스화한다
+    form = UserForm()
+    # 폼의 값을 검증한다
+    if form.validate_on_submit():
+        # 사용자를 작성한다
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # 사용자를 추가하고 커밋한다
+        db.session.add(user)
+        db.session.commit()
+        # 사용자의 일람 화면으로 리다이렉트한다
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
+
+@crud.route("/users")
+def users():
+    """사용자의 일람을 취득한다"""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+# methods에 GET과 POST를 지정한다
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+
+    # User 모델을 이용하여 사용자를 취득한다
+    user = User.query.filter_by(id=user_id).first()
+
+    # form으로부터 제출된 경우는 사용자를 갱신하여 사용자의 일람 화면으로 리다이렉트한다
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+    # GET의 경우는 HTML을 반환한다
+    return render_template("crud/edit.html", user=user, form=form)
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user=User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
